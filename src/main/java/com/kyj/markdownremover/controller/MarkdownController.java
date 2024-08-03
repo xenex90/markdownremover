4bf0203c310b30da409990602f875e7121839924
xenex90 <xenexes@naver.com>
2024-08-03 14:57:06 +0900

    개발 도중 브랜치 중간 커밋 및 푸쉬

--- src/main/java/com/kyj/markdownremover/controller/MarkdownController.java
+++ src/main/java/com/kyj/markdownremover/controller/MarkdownController.java
@@ -7,11 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 @Controller
@@ -34,76 +37,100 @@ public class MarkdownController {
 
 
         StringBuilder plainText = new StringBuilder();
-
+        int count = 0;
+        LinkedHashMap countedMap = new LinkedHashMap();
+        HashMap checkMap  = new HashMap<>();
         for (String line : lines) {
-            plainText.append(removeMarkdownFromLine(line)).append("\n");
+            boolean checked = false;
+            checkMap = removeMarkdownFromLine(line);
+            Set<String> keys = checkMap.keySet();
+            String tmpString = null;
+
+            for (String key : keys) {
+                tmpString = key;
+            }
+
+            count++;
+
+            plainText.append(tmpString).append("\n");
+
+            countedMap.put(checkMap, count);
         }
+        count = 0;
         logger.info("---- 플레인 텍스트 ----");
         logger.info(String.valueOf(plainText));
         logger.info("---- 플레인 텍스트 ---- 끝");
 
-        model.addAttribute("markdownText",markdownText);
-        model.addAttribute("plainText",plainText);
+
+
+        model.addAttribute("markdownText", markdownText);
+        model.addAttribute("plainText", plainText);
+        model.addAttribute("countedMap", countedMap);
 
 
         return "markdown/markdownView";
     }
 
-    private static String removeMarkdownFromLine(String line) {
+    private static HashMap<String, Boolean> removeMarkdownFromLine(String line) {
+        HashMap map =  new HashMap<String, Boolean>();
         // Remove headers
         if (line.startsWith("#")) {
-            return line.replaceAll("#", "").trim();
+            line = line.replaceAll("#", "").trim();
+            map.put(line, true);
+            return map;
         }
 
         // Remove bold and italic
-        line = line.replaceAll("\\*\\*|__", "");
-        line = line.replaceAll("\\*|_", "");
+        line = line.replaceAll("\\*\\*|__", "").trim();
+        line = line.replaceAll("\\*|_", "").trim();
 
         // Remove blockquotes
         if (line.startsWith(">")) {
-            return line.substring(1).trim();
+            map.put(line.substring(1).trim(), true);
+            return map;
         }
 
         // Remove ordered list numbers
         if (line.matches("^\\d+\\.\\s.*")) {
-            return line.substring(line.indexOf('.') + 1).trim();
+            map.put(line.substring(line.indexOf('.') + 1).trim(), true);
+            return map;
         }
 
         // Remove code block markers
         if (line.contains("```")) {
-            return line.replaceAll("```", "");
+            map.put(line.replaceAll("```", "").trim(), true);
+            return  map;
         }
 
         // Remove images and links
         if (line.matches("!\\[.*\\]\\(.*\\)") || line.matches("\\[.*\\]\\(.*\\)")) {
-            return line.replaceAll("\\[.*\\]\\(.*\\)", "");
+            map.put(line.replaceAll("\\[.*\\]\\(.*\\)", "").trim(), true);
+            return map;
         }
 
         if (line.contains("~~")) {
-            return line.replaceAll("~~", "");
+            map.put(line.replaceAll("~~", "").trim(), true);
+            return map;
         }
 
         if (line.contains("- ") && !line.contains("|")) {
-            return line.replaceAll("- ", "");
+            map.put(line.replaceAll("~~", "").trim(), true);
+            return map;
         }
 
         if (line.contains("---") && !line.contains("|")) {
-            return line.replaceAll("---", "");
+            map.put(line.replaceAll("---", "").trim(), true);
+            return map;
         }
 
         try {
-//            logger.info(line);
-            line = removeTableFormat.removeTableMethod(line);
+
+            map = removeTableFormat.removeTableMethod(line);
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        if (line.contains("|")) {
-//            while (line.contains("|")) {
-//                line = line.replace("|", "").replace("-", "");
-//            }
-//        }
 
-        return line;
+        return map;
     }
 }
 
--- src/main/java/com/kyj/markdownremover/tableRemover/removeTableFormat.java
+++ src/main/java/com/kyj/markdownremover/tableRemover/removeTableFormat.java
@@ -7,18 +7,19 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.LinkedHashMap;
 
 public class removeTableFormat {
 
     private static final Logger logger = LoggerFactory.getLogger(MarkdownremoverApplication.class);
 
-    public static String removeTableMethod(String text) {
+    public static LinkedHashMap removeTableMethod(String text) {
         // 정규식을 사용하여 테이블 형식 제거
         text = text.replaceAll("\\|\\s*:?[-]+\\s*:?\\|\\s*:?[-]+\\s*:?\\|\\s*:?[-]+\\s*:?\\|", "").replace("\r","").replace("\t","");
         BufferedReader reader = new BufferedReader(new StringReader(text));
         StringBuilder result = new StringBuilder();
         String line;
-//        logger.info(text);
+        LinkedHashMap<String, Boolean> map = new LinkedHashMap<String, Boolean>();
         try {
             while ((line = reader.readLine()) != null && !line.isEmpty()) {
                 // 테이블 구분선이 아닌 라인 처리
@@ -29,14 +30,15 @@ public class removeTableFormat {
                     line = line.replaceAll("\\s*\\|\\s*", " ");
 
                 }
+                map.put(line,true);
                 result.append(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        map.put(result.toString(), true);
         // 최종 결과 문자열 반환
-        return result.toString();
+        return map;
     }
 }
 
